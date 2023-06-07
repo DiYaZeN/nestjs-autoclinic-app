16109bba883c31905e5a1c88a8fab961a20e07e7
DiYaZeN <minolamarakoon@gmail.com>
2023-06-07 20:00:07 +0530

    Git-Init

---
+++ src/Database/database.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm/connection/Connection';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'MINult$123',
+      database: 'autoclinic_car _rental',
+      entities: [],
+      synchronize: true,
+      extra: {
+        insecureAuth: true,
+      },
+    }),
+  ],
+
+  exports: [TypeOrmModule],
+})
+export class DatabaseModule {
+  constructor(private connection: Connection) {
+    if (connection.isConnected) {
+      console.log('Database is connected');
+    } else {
+      console.log('Database is not connected');
+    }
+  }
+}
