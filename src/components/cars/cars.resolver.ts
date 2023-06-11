9307f112f14d5272c84ea9041366a6588725804c
DiYaZeN <minolamarakoon@gmail.com>
2023-06-11 17:34:08 +0530

    graohql1

--- src/components/cars/cars.resolver.ts
+++ src/components/cars/cars.resolver.ts
@@ -1,11 +1,16 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Mutation, Query, Resolver } from '@nestjs/graphql';
 import { CarsService } from './cars.service';
+import { Car } from './entities/car';
 
 @Resolver()
 export class CarsResolver {
   constructor(private carsService: CarsService) {}
-  @Query((returns) => String)
-  public async cars() {
-    return 'Hello World! cars';
+  @Query((returns) => [Car])
+  public async cars(): Promise<Car[]> {
+    return await this.carsService.getAllCars().catch((err) => {
+      throw err;
+    });
   }
+
+  // @Mutation((returns) => Car)
 }
